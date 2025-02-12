fe7b763bf4f6f1360aaf5d54ff5d75e966230066
Javier Arguello <javier@qurable.co>
2025-02-12 16:19:38 +0100

    fix fonts folder

--- src/libs/video/ffmpeg.ts
+++ src/libs/video/ffmpeg.ts
@@ -207,7 +207,7 @@ export class FFmpegProcessor {
       '-movflags',
       '+faststart',
       '-vf',
-      `subtitles=${tempSubtitlePath}:fontsdir=/ffmpeg-fonts`,
+      `subtitles=${tempSubtitlePath}:fontsdir=/ffmpeg_fonts`,
       outputVideoFilename,
     ];
 
